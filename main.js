7955120b541d30e225f83df8d9ef4a9042f6fdef
TilenVSS <56862535+Floralyz@users.noreply.github.com>
2023-01-21 16:47:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+
+console.log('Hello from main.js')
+
+//variable
+/*
+let age = 25
+
+console.log(age)
+
+const salary = 8000
+
+console.log(salary)
+*/
+/*
+const a = 'Vishwas'
+const language = "JavaScript"
+
+const total = 0
+const PI = 3.14
+
+const isPrimaryNumber = true
+const isNewUser = false
+
+let result
+console.log(result)
+
+const res = null
+
+const data = null
+
+const person = {
+    'firstName':"Bruce",
+    'lastName':"Wayne",
+    'age':30
+}
+
+
+console.log(person.lastName)
+
+const oddNumbers = [1, 3, 5, 7, 9]
+
+console.log(oddNumbers[3])
+
+*/
+/*
+let x = 10
+let y = 5
+console.log(x+y)
+console.log(++x)
+
+
+console.log(x!=y)
+
+const isValidNumber = x > 8 || 8 > y
+console.log(isValidNumber)
+
+
+const isValid = true
+console.log(!isValid)
+
+
+console.log('Bruce ' + ' Wayne')
+
+const isEven = 10 % 2 === 0 ? true:false
+console.log(isEven)
+
+*/
+/*
+console.log(2 + '3')
+console.log('4' - '2')
+console.log('5'-null)
+*/
+/*
+console.log(parseFloat('5.43'))
+
+console.log(String(500))
+console.log((500).toString())
+console.log(Boolean(10))
+*/
+/*
+const var1 = 'test'
+const var2 = 'test'
+
+console.log(var1 == var2)
+console.log(var1 == var2)
+*/
+/*
+function greet(username, surname){
+    console.log('Good morning' + username + ' ' + surname)
+}
+
+greet(' Bruce', 'Wayne')
+
+
+function add(a,b){
+    return a + b 
+}
+
+const arrowSum = (a,b) =>  a+b
+const addFive = (num) => num+5
+
+const sum = arrowSum(3,4)
+console.log(sum)
+*/
+
+const myNum = 100
+
+if(true){
+    const myName = 'Vishnas'
+    console.log(myName)
+    console.log(myNum)
+}
+
+function testFn(){
+    const myName = 'Batman'
+    console.log(myName)
+}
+
+testFn()
+
